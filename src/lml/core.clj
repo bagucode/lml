14b5bc58c4518cee7f1a9ca1dd3fb380fad70cbd
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2014-07-06 13:35:55 +0200

    WIP

--- src/lml/core.clj
+++ src/lml/core.clj
@@ -1,6 +1,25 @@
 (ns lml.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-call [form]
+       (= 'def tok)    (parse-def tok-seq)
+       (= 'extern tok) (parse-extern tok-seq)
+  )
+
+(defn parse-def [form]
+  )
+
+(defn parse [tok-seq]
+  (when-let [tok (first tok-seq)]
+    (lazy-seq
+     (cons
+      (cond
+       (seq? tok)      (parse-call tok-seq)
+       (number? tok)   {:tok :number, :val tok}
+       (symbol? tok)   {:tok :identifier, :val tok}
+       :else           {:tok :illegal, :val tok})
+      (parse (next tok-seq))))))
+
+(defn gettok [tok-seq]
+  (when-let [tok (first tok-seq)]
+    ))
+
