871b29c07aea619eecde016a003fea080a115736
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2014-07-13 18:11:48 +0200

    sdfgsdfg

--- src/lml/core.clj
+++ src/lml/core.clj
@@ -24,3 +24,18 @@
   (when-let [tok (first tok-seq)]
     ))
 
+;; # Compute the x'th fibonacci number.
+;; def fib(x)
+;;   if x < 3 then
+;;     1
+;;   else
+;;     fib(x-1)+fib(x-2)
+
+;; # This expression will compute the 40th number.
+;; fib(40)
+
+;; (def fib (x)
+;;   (if (< x 3)
+;;     1
+;;     (+ (fib (- x 1)) (fib (- x 2)))))
+
